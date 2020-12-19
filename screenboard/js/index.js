3321dfb5a0c4e56a2a97e923e9f813cd8aea8535
Zhaofan-Su <1015655239@qq.com>
2020-12-20 00:32:42 +0800

    delete some code

--- screenboard/js/index.js
+++ screenboard/js/index.js
@@ -61,6 +61,7 @@ $(function () {
             }, 1)
         }
     }
+
     var k = new AMap.Map("myMap", {
         resizeEnable: true,
         zoom: 17,
@@ -77,7 +78,6 @@ $(function () {
         [113.964750, 22.586636],
         [113.964406, 22.585858]
     ];
-    var f;
     for (var d = 0; d < n.length; d += 1) {
         var g = new AMap.Marker({
             position: n[d],
@@ -217,7 +217,6 @@ $(function () {
         j.setOption(l)
     }, 500);
     var u, v, w, r, t;
-    var m;
 
     v = echarts.init(document.getElementById("summaryPie2"));
     var A = {
@@ -300,446 +299,7 @@ $(function () {
     };
     v.setOption(C);
 
-    function p() {
-        u = echarts.init(document.getElementById("summaryPie1"));
-        v = echarts.init(document.getElementById("summaryPie2"));
-        w = echarts.init(document.getElementById("summaryPie3"));
-        var E = $(window).width();
-        var A;
-        if (E > 1600) {
-            A = {
-                pieTop: "40%",
-                pieTop2: "36%",
-                titleSize: 20,
-                pieRadius: [80, 85],
-                itemSize: 32
-            }
-        } else {
-            A = {
-                pieTop: "30%",
-                pieTop2: "26%",
-                titleSize: 16,
-                pieRadius: [60, 64],
-                itemSize: 28
-            }
-        }
-        var B = {
-            title: {
-                x: "center",
-                y: A.pieTop,
-                text: "司机",
-                textStyle: {
-                    fontWeight: "normal",
-                    color: "#ffd325",
-                    fontSize: A.titleSize,
-                },
-                subtext: "总数：100人\n今日工作：25人",
-                subtextStyle: {
-                    color: "#fff",
-                }
-            },
-            tooltip: {
-                show: false,
-            },
-            toolbox: {
-                show: false,
-            },
-            series: [{
-                type: "pie",
-                clockWise: false,
-                radius: A.pieRadius,
-                hoverAnimation: false,
-                center: ["50%", "50%"],
-                data: [{
-                    value: 25,
-                    label: {
-                        normal: {
-                            formatter: "{d}%",
-                            position: "outside",
-                            show: true,
-                            textStyle: {
-                                fontSize: A.itemSize,
-                                fontWeight: "normal",
-                                color: "#ffd325"
-                            }
-                        }
-                    },
-                    itemStyle: {
-                        normal: {
-                            color: "#ffd325",
-                            shadowColor: "#ffd325",
-                            shadowBlur: 10
-                        }
-                    }
-                }, {
-                    value: 75,
-                    name: "未工作",
-                    itemStyle: {
-                        normal: {
-                            color: "rgba(44,59,70,1)",
-                            label: {
-                                show: false
-                            },
-                            labelLine: {
-                                show: false
-                            }
-                        },
-                        emphasis: {
-                            color: "rgba(44,59,70,1)"
-                        }
-                    },
-                    itemStyle: {
-                        normal: {
-                            color: "#11284e",
-                            shadowColor: "#11284e",
-                        }
-                    },
-                }]
-            }]
-        };
-        var C = {
-            title: {
-                x: "center",
-                y: A.pieTop,
-                text: "车辆",
-                textStyle: {
-                    fontWeight: "normal",
-                    color: "#32ffc7",
-                    fontSize: A.titleSize
-                },
-                subtext: "总数：100辆\n今日工作：75辆人",
-                subtextStyle: {
-                    color: "#fff",
-                }
-            },
-            tooltip: {
-                show: false,
-            },
-            toolbox: {
-                show: false,
-            },
-            series: [{
-                type: "pie",
-                clockWise: false,
-                radius: A.pieRadius,
-                hoverAnimation: false,
-                center: ["50%", "50%"],
-                data: [{
-                    value: 75,
-                    label: {
-                        normal: {
-                            formatter: "{d}%",
-                            position: "outside",
-                            show: true,
-                            textStyle: {
-                                fontSize: A.itemSize,
-                                fontWeight: "normal",
-                                color: "#32ffc7"
-                            }
-                        }
-                    },
-                    itemStyle: {
-                        normal: {
-                            color: "#32ffc7",
-                            shadowColor: "#32ffc7",
-                            shadowBlur: 10
-                        }
-                    }
-                }, {
-                    value: 25,
-                    name: "未工作",
-                    itemStyle: {
-                        normal: {
-                            color: "rgba(44,59,70,1)",
-                            label: {
-                                show: false
-                            },
-                            labelLine: {
-                                show: false
-                            }
-                        },
-                        emphasis: {
-                            color: "rgba(44,59,70,1)"
-                        }
-                    },
-                    itemStyle: {
-                        normal: {
-                            color: "#11284e",
-                            shadowColor: "#11284e",
-                        }
-                    },
-                }]
-            }]
-        };
-        var D = {
-            title: {
-                x: "center",
-                y: A.pieTop2,
-                text: "运单",
-                textStyle: {
-                    fontWeight: "normal",
-                    color: "#1eb6fe",
-                    fontSize: A.titleSize
-                },
-                subtext: "总数：100单\n正常单：50单\n异常单：50单",
-                subtextStyle: {
-                    color: "#fff",
-                }
-            },
-            tooltip: {
-                show: false,
-            },
-            toolbox: {
-                show: false,
-            },
-            series: [{
-                type: "pie",
-                clockWise: false,
-                radius: A.pieRadius,
-                hoverAnimation: false,
-                center: ["50%", "50%"],
-                data: [{
-                    value: 50,
-                    label: {
-                        normal: {
-                            formatter: "{d}%",
-                            position: "outside",
-                            show: true,
-                            textStyle: {
-                                fontSize: A.itemSize,
-                                fontWeight: "normal",
-                                color: "#1eb6fe"
-                            }
-                        }
-                    },
-                    itemStyle: {
-                        normal: {
-                            color: "#1eb6fe",
-                            shadowColor: "#1eb6fe",
-                            shadowBlur: 10
-                        }
-                    }
-                }, {
-                    value: 50,
-                    name: "未工作",
-                    itemStyle: {
-                        normal: {
-                            color: "rgba(44,59,70,1)",
-                            label: {
-                                show: false
-                            },
-                            labelLine: {
-                                show: false
-                            }
-                        },
-                        emphasis: {
-                            color: "rgba(44,59,70,1)"
-                        }
-                    },
-                    itemStyle: {
-                        normal: {
-                            color: "#11284e",
-                            shadowColor: "#11284e",
-                        }
-                    },
-                }]
-            }]
-        };
-        r = echarts.init(document.getElementById("summaryBar"));
-        var i = {
-            tooltip: {
-                trigger: "item",
-                formatter: function (F) {
-                    var G = "本月" + F.name + "号运单数：" + F.data;
-                    return G
-                }
-            },
-            grid: {
-                top: "20%",
-                left: "15%",
-                width: "80%",
-                height: "80%",
-                containLabel: true
-            },
-            xAxis: {
-                data: ["美的南沙分厂", "美的商业空调事业部", "佛山信华"],
-                axisLabel: {
-                    show: true,
-                    textStyle: {
-                        fontSize: "12px",
-                        color: "#fff",
-                    }
-                },
-                axisLine: {
-                    lineStyle: {
-                        color: "#fff",
-                        width: 1,
-                    }
-                }
-            },
-            yAxis: {
-                axisLabel: {
-                    show: true,
-                    textStyle: {
-                        fontSize: "12px",
-                        color: "#fff",
-                    }
-                },
-                axisLine: {
-                    lineStyle: {
-                        color: "#fff",
-                        width: 1,
-                    }
-                },
-                splitLine: {
-                    show: false,
-                }
-            },
-            series: {
-                name: "",
-                type: "bar",
-                barWidth: 20,
-                data: ["15", "13", "17"],
-                itemStyle: {
-                    normal: {
-                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                            offset: 0,
-                            color: "#3876cd"
-                        }, {
-                            offset: 0.5,
-                            color: "#45b4e7"
-                        }, {
-                            offset: 1,
-                            color: "#54ffff"
-                        }]),
-                    },
-                },
-            },
-        };
-        t = echarts.init(document.getElementById("summaryLine"));
-        var z = {
-            tooltip: {
-                trigger: "item",
-                formatter: function (F) {
-                    var G = "本月" + F.name + "号运单数：" + F.data;
-                    return G
-                }
-            },
-            grid: {
-                top: "20%",
-                left: "0%",
-                width: "100%",
-                height: "80%",
-                containLabel: true
-            },
-            xAxis: {
-                data: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"],
-                axisLabel: {
-                    show: true,
-                    textStyle: {
-                        fontSize: "12px",
-                        color: "#3e70b0",
-                    }
-                },
-                axisLine: {
-                    lineStyle: {
-                        color: "#0e2c52",
-                        width: 1,
-                    }
-                }
-            },
-            yAxis: {
-                axisLabel: {
-                    show: true,
-                    textStyle: {
-                        fontSize: "12px",
-                        color: "#3e70b0",
-                    }
-                },
-                axisLine: {
-                    lineStyle: {
-                        color: "#0e2c52",
-                        width: 1,
-                    }
-                },
-                splitLine: {
-                    show: true,
-                    lineStyle: {
-                        color: "#0e2c52",
-                        width: 1,
-                    }
-                }
-            },
-            series: {
-                name: "",
-                type: "line",
-                data: ["5", "14", "3", "6", "8", "18", "11", "4", "8", "7", "16", "13", "6", "10", "11", "9", "19", "13", "4", "20", "12", "7", "13", "15", "8", "3", "9", "16", "11", "16", "8"],
-                areaStyle: {
-                    normal: {
-                        color: "rgba(79,237,247,0.3)",
-                    }
-                },
-                itemStyle: {
-                    normal: {
-                        lineStyle: {
-                            color: "#00dafb",
-                            width: 1,
-                        },
-                        color: "#00dafb",
-                    },
-                },
-            },
-        };
-        u.setOption(B);
-        v.setOption(C);
-        w.setOption(D);
-        r.setOption(i);
-        t.setOption(z)
-    }
 
-
-    $(".summaryBtn").on("click", function () {
-        $(".filterbg").show();
-        $(".popup").show();
-        $(".popup").width("3px");
-        $(".popup").animate({
-            height: "76%"
-        }, 400, function () {
-            $(".popup").animate({
-                width: "82%"
-            }, 400)
-        });
-        setTimeout(x, 800)
-    });
-    $(".popupClose").on("click", function () {
-        $(".popupClose").css("display", "none");
-        $(".summary").hide();
-        u.clear();
-        v.clear();
-        w.clear();
-        r.clear();
-        t.clear();
-        $(".popup").animate({
-            width: "3px"
-        }, 400, function () {
-            $(".popup").animate({
-                height: 0
-            }, 400)
-        });
-        setTimeout(s, 800)
-    });
-
-    function x() {
-        $(".popupClose").css("display", "block");
-        $(".summary").show();
-        p()
-    }
-
-    function s() {
-        $(".filterbg").hide();
-        $(".popup").hide();
-        $(".popup").width(0)
-    }
     $(window).resize(function () {
         j.resize();
         try {
@@ -769,15 +329,7 @@ $(function () {
             $(".searchBtn").show()
         }, 400)
     });
-    $(".stateUl").niceScroll({
-        cursorcolor: "#045978",
-        cursoropacitymax: 0.6,
-        touchbehavior: false,
-        cursorwidth: "4px",
-        cursorborder: "0",
-        cursorborderradius: "4px",
-        autohidemode: false
-    });
+
     var a = [{
         dateLable: "2018-01-01 星期一",
         data: {
@@ -919,7 +471,6 @@ $(function () {
     }];
 
     function o() {
-        var z = $(".dataBox");
         var B = z.eq(0).width();
         var A = 24 * 60;
 
@@ -928,25 +479,7 @@ $(function () {
             var D = parseInt(E[0]) * 60 + parseInt(E[1]);
             return D
         }
-        z.each(function (F, E) {
-            var G = a[F];
 
-            function D(J, H) {
-                for (var I = 0; I < H.length; I++) {
-                    var K, P, Q, M, O, N, L;
-                    K = i(H[I].start) / A * 100 + "%";
-                    P = (i(H[I].end) - i(H[I].start)) / A * 100 + "%";
-                    M = G.dateLable;
-                    O = H[I].start;
-                    N = H[I].end;
-                    L = i(H[I].end) - i(H[I].start);
-                    Q = '<span style="width: ' + P + ";left: " + K + '" sDate="' + M + '" sStart="' + O + '" sEnd="' + N + '" sConsume="' + L + '"></span>';
-                    J.append(Q)
-                }
-            }
-            D($(E).find(".workTime"), G.data.workTime);
-            D($(E).find(".standard"), G.data.standard)
-        });
         var C = $(".totalItem");
         C.each(function (E, D) {
             var F = a[E].data.workTime;
@@ -960,52 +493,4 @@ $(function () {
         })
     }
     o();
-    $(".workTime").on("mouseenter", "span", function (i) {
-        var G = i.clientX;
-        var H = i.clientY;
-        var B, F, C, A, z, D, E;
-        B = $(this).attr("sDate");
-        F = $(this).attr("sStart");
-        C = $(this).attr("sEnd");
-        A = $(this).attr("sConsume");
-        D = Math.floor(A / 60);
-        E = A - D * 60;
-        z = '<div class="workTimeInfo" style="left:' + G + "px;top:" + H + 'px"><p>日期：' + B + "</p><p>开始时间：" + F + "</p><p>结束时间：" + C + "</p><p>总用时：" + D + "小时" + E + "分钟</p></div>";
-        $("body").append(z)
-    });
-    $(".workTime").on("mouseout", function () {
-        $(".workTimeInfo").remove()
-    });
-    $(".infoBtn").on("click", function () {
-        $(".filterbg").show();
-        $(".carInfo").show();
-        $(".carInfo").width("3px");
-        $(".carInfo").animate({
-            height: "76%"
-        }, 400, function () {
-            $(".carInfo").animate({
-                width: "82%"
-            }, 400)
-        });
-        setTimeout(function () {
-            $(".infoBox").show();
-            $(".carClose").css("display", "block")
-        }, 800)
-    });
-    $(".carClose").on("click", function () {
-        $(".carClose").css("display", "none");
-        $(".infoBox").hide();
-        $(".carInfo").animate({
-            width: "3px"
-        }, 400, function () {
-            $(".carInfo").animate({
-                height: 0
-            }, 400)
-        });
-        setTimeout(function () {
-            $(".filterbg").hide();
-            $(".carInfo").hide();
-            $(".carInfo").width(0)
-        }, 800)
-    })
 });
